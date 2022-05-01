33222de28de948f7179216750d3b264a7ce3a20d
Gerrit Touch Laptop <gerritcloete.gj@gmail.com>
2022-05-01 15:52:44 +0200

    Recent posts component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,20 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { Key } from 'react'
 
 import { Categories , PostCard , PostWidget} from '../components'
 
-
-const posts = [
-  {title: "Post Title", excerpt: "Post Excerpt"},
-  {title: "Post Title 2", excerpt: "Post Excerpt 2"}
-]; //demo post
+import { getPosts } from '../services'
 
 
-const Home: NextPage = () => {
+// const posts = [
+//   {title: "Post Title", excerpt: "Post Excerpt"},
+//   {title: "Post Title 2", excerpt: "Post Excerpt 2"}
+// ]; //demo post not required
+
+
+const Home: NextPage<{posts: any}> = ({ posts }) => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -21,9 +24,7 @@ const Home: NextPage = () => {
 
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'> 
         <div className='lg:col-span-8 col-span-1'>
-          { posts.map((post) => ( //map through posts and make a card for each
-            <PostCard post={post} key={post.title} />
-          ))}
+          { posts.map((post: { node: any },index: Key | null | undefined) => <PostCard key={index} post={post.node}/> )}
         </div>
         <div className="lg:col-span-4 col-span-1"> 
           <div className="lg:sticky relative top-8">
@@ -33,11 +34,16 @@ const Home: NextPage = () => {
         </div>
       </div> 
 
-
-
-
     </div>
   )
 }
 
-export default Home
+export default Home;
+
+//next.js method to get props
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: {posts}
+  }
+}
--- services/index.js
+++ services/index.js
@@ -1,8 +1,82 @@
+import { graphql } from "graphql";
 import { request, gql } from "graphql-request";
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHQL_API; 
 
 export const getPosts = async () => {
     const query = gql`
         query MyQuery {
-        
+            postsConnection {
+            edges {
+                node {
+                author {
+                    bio
+                    id
+                    name
+                    photo {
+                    url
+                    }
+                }
+                createdAt
+                slug
+                title
+                excerpt
+                featuredImage {
+                    url
+                }
+                categories {
+                    name
+                    slug
+                }
+                id
+                }
+            }
+            }
         }
-    '
+    `
+    const results = await request(graphqlAPI, query);
+
+    return results.postsConnection.edges;
+}
+
+export const getRecentPosts = async () => {
+    const query = gql`
+        query GetPostDetails() {
+            posts(
+                orderBy: createdAt_ASC,
+                last: 3
+            ) {
+                title
+                featuredImage {
+                    url
+                }
+                createdAt
+                slug
+            }
+        }
+    `
+    const results = await request(graphqlAPI, query);
+
+    return results.posts;
+}
+
+export const getSimilarPosts = async () => {
+    const query = gql`
+        query GetPostDetails($slug: String!, $categories: [String!]) {
+            posts(
+                where: {slug_not: $slug, AND: categories_some: {slug_in: $categories}},
+                last: 3
+            ){
+                title
+                featuredImage {
+                    url
+                }
+                createdAt
+                slug
+            }
+        }
+    `
+    const results = await request(graphqlAPI, query);
+
+    return results.posts;
+}
