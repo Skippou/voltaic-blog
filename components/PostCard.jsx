2904c5ee0fa476a80f7f794ddb323f2a440dbc99
Gerrit Touch Laptop <gerritcloete.gj@gmail.com>
2022-05-03 18:59:39 +0200

    Ready to deploy

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -10,7 +10,7 @@ const PostCard = ({ post }) => {
         <img
           src={post.featuredImage.url}
           alt={post.title}
-          className='object-scale-down absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg'
+          className='object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg'
         />
 
       </div>
