ab02649741369a6f55fd5aa7be1e9b59cd228c86
Gerrit Touch Laptop <gerritcloete.gj@gmail.com>
2022-05-05 16:13:26 +0200

    Revert "Try /vercel/next.js/tree/canary/examples/gh-pages"
    This reverts commit fe791c16763185cf7d8a6c20c60025d354daa3db.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const debug = process.env.NODE_ENV !== 'production'
-
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: !debug ? '/voltaic-blog/' : '',
 }
